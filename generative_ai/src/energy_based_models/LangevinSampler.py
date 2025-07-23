c325011538da2204ae074e61fa33002e3e02fbd0
Ilan <46520945+Ilan123@users.noreply.github.com>
2025-07-23 17:21:54 +0300

    Update: Remove unnecessary import- Buffer
    Remove the import of Buffer class from the Buffer module

--- generative_ai/src/energy_based_models/LangevinSampler.py
+++ generative_ai/src/energy_based_models/LangevinSampler.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn, Tensor
-from Buffer import Buffer, AbstractBuffer
+from Buffer import AbstractBuffer
 from typing import Tuple, Union
 import math
 
@@ -31,7 +31,7 @@ class LangevinSampler:
         self._model = model
         self._default_n_steps = default_n_steps
         self._default_step_size = default_step_size
-        self._buffer = buffer
+        self._ = 
         self._clip_grad = clip_grad
         self._clip_sample = clip_sample
         self._was_param_freazed = {}
